dcf88a89b027d2a0a95048e6ec7ee9f903cbd5ad
Hafiz Mudassir <malikjrw147@gmail.com>
2019-11-24 00:29:08 +0500

    Question#8 completed

--- assignment.cpp
+++ assignment.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 int main() 
 {
-int num;
-cout<<"Please enter a number:";
-cin>>num;
-cout<<"Square of your entered number:"<< num * num;
-cout<<"\nCube of your entered number:"<< num * num * num;
+float total, pages_Read_In_One_Day, days_taken, remainig_pages, read_pages;
+cout<<"Please enter toatal pages of a book:";cin>>total;
+cout<<"Please enter number of pages read in one day:";cin>>pages_Read_In_One_Day;
+cout<<"Please enter number of days a person has read the book:";cin>>days_taken;
+read_pages = days_taken * pages_Read_In_One_Day;
+remainig_pages = total - read_pages;
+cout<<"Pages that have been read are:"<<read_pages;
+cout<<"\nPages that are remaining:"<<remainig_pages;
 return 0;
 }
