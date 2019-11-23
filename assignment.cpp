d1e4953321dbeda479f8a5f48de089def9cd6850
Hafiz Mudassir <malikjrw147@gmail.com>
2019-11-23 23:56:13 +0500

    Question#3 Completed

--- assignment.cpp
+++ assignment.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 int main() 
 {
-float rad, vol, area, circum;
-cout <<"Please enter radius of sphere :";
-cin>>rad;
-area = 4*3.14*rad*rad;
-circum = (4/3)*3.14*rad*rad*rad;
-cout <<"\n Area of sphere is :"<<area<<endl;
-cout<<"Circumference and Volume of sphere is:"<<circum<<endl;
+float a, b, c, s, area;
+cout<<"Please enter the value of sides of triangle: \n Enter the value of side a:";
+cin>>a;
+cout<<endl;
+cout<<"Enter the value of side b:"; cin>>b; cout<<endl;
+cout<<"Enter the value of side c:"; cin>>c; cout<<endl;
+s = (a + b + c ) / 2;
+area = s*(s-a) * (s-b) * (s-c);
+cout<<"Area of the triangle is:" << area;
 return 0;
 }
