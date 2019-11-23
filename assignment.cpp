4ef6c2669531a93cc92bde736755b06069b458b4
Hafiz Mudassir <malikjrw147@gmail.com>
2019-11-23 23:33:08 +0500

    Question 1 Completed

---
+++ assignment.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main() 
+{
+int a = 12345;
+float b = 1.546;
+char name[] = "Kashi";
+cout <<"This text for assignment :"<< endl;
+cout <<"Integer Value :" << a << endl;
+cout <<"Float Value :" << b << endl;
+cout <<"Character Value :" << name << endl;
+}
