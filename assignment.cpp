169ccde6f4604dd2ae0d8383932d1bb738b1123c
Hafiz Mudassir <malikjrw147@gmail.com>
2019-11-24 00:09:54 +0500

    Question#5 completed

--- assignment.cpp
+++ assignment.cpp
@@ -3,10 +3,21 @@
 using namespace std;
 int main() 
 {
-float miles, km;
-cout<<"Enter the distance in Miles:"; 
-cin>>miles;
-km = 1.609 * miles;
-cout<<"Value of kilometer is:" << km << endl;
+	int i,j,k,l;
+float sum, avrg, product;
+cout<<"Enter four numbers below \nenter first number:"; 
+cin>>i;
+cout<<"\n enter second number:"; 
+cin>>j;
+cout<<"\n enter third number:"; 
+cin>>k;
+cout<<"\n enter fourth number:"; 
+cin>>l;
+sum = i+j+k+l;
+product = i*j*k*l;
+avrg = ( i+j+k+l )/4;
+cout<<"\n Sum of entered numbers is:"<<sum;
+cout<<"\n Product of entered numbers is:"<<product;
+cout<<"\n Average of entered numbers is:"<<avrg;
 return 0;
 }
