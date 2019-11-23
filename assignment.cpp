a567ffa4ed7384e1aeaeaaddb417eb347a4d9ae5
Hafiz Mudassir <malikjrw147@gmail.com>
2019-11-24 00:18:23 +0500

    Question#7 completed

--- assignment.cpp
+++ assignment.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 int main() 
 {
-float year, month, day;
-cout<<"How old are you? Enter your age in years:";
-cin>>year;
-month=12*year;
-day=365*year;
-cout<<"\n Your age in months is:"<<month;
-cout<<"\n Your age in days is:"<<day;
+int num;
+cout<<"Please enter a number:";
+cin>>num;
+cout<<"Square of your entered number:"<< num * num;
+cout<<"\nCube of your entered number:"<< num * num * num;
 return 0;
 }
