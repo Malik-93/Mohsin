fd6b5c4a0adafc1e65afc8755bbb514f723e1f67
Hafiz Mudassir <malikjrw147@gmail.com>
2019-11-24 00:01:21 +0500

    Question#4 completed

--- assignment.cpp
+++ assignment.cpp
@@ -3,14 +3,10 @@
 using namespace std;
 int main() 
 {
-float a, b, c, s, area;
-cout<<"Please enter the value of sides of triangle: \n Enter the value of side a:";
-cin>>a;
-cout<<endl;
-cout<<"Enter the value of side b:"; cin>>b; cout<<endl;
-cout<<"Enter the value of side c:"; cin>>c; cout<<endl;
-s = (a + b + c ) / 2;
-area = s*(s-a) * (s-b) * (s-c);
-cout<<"Area of the triangle is:" << area;
+float miles, km;
+cout<<"Enter the distance in Miles:"; 
+cin>>miles;
+km = 1.609 * miles;
+cout<<"Value of kilometer is:" << km << endl;
 return 0;
 }
